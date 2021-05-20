c59f1cbcdfcfce5bd1cb2982d696ddb224550a70
Sunky97 <hsk11261612@gmail.com>
2021-05-21 02:49:28 +0900

    게시물조회 모듈 추가

--- src/main/java/com/sunky/gallery/controller/BoardController.java
+++ src/main/java/com/sunky/gallery/controller/BoardController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,25 +22,36 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model model){
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
 
-        log.info("-------------pagerequest"+pageRequestDTO);
-        log.info("-------------"+boardService.getList(pageRequestDTO));
+        log.info("-------------pagerequest" + pageRequestDTO);
+        log.info("-------------" + boardService.getList(pageRequestDTO));
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
     }
 
     @GetMapping("/register")
-    public void register(){
+    public void register() {
         log.info("register get...");
     }
 
     @PostMapping("/register")
-    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
-        log.info("dto..."+dto);
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
         Long pno = boardService.register(dto);
-        redirectAttributes.addFlashAttribute("msg",pno);
+        redirectAttributes.addFlashAttribute("msg", pno);
 
         return "redirect:/board/list";
     }
+
+    @GetMapping("/read")
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long pno, Model model) {
+
+        log.info("pno: " + pno);
+
+        BoardDTO boardDTO = boardService.get(pno);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
 }
