bc8859b5700305d948285bb4019e564072fe1330
Sunky97 <hsk11261612@gmail.com>
2021-05-20 15:07:24 +0900

    ReplyRepository 등록

---
+++ src/main/java/com/sunky/gallery/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.sunky.gallery.repository;
+
+import com.sunky.gallery.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository <Reply, Long>{
+}
