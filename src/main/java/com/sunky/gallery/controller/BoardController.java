eca035fc954d4db0330b4ad3db747c4825c30502
Sunky97 <hsk11261612@gmail.com>
2021-05-21 02:39:34 +0900

    register 모듈 등록

--- src/main/java/com/sunky/gallery/controller/BoardController.java
+++ src/main/java/com/sunky/gallery/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.sunky.gallery.controller;
 
+import com.sunky.gallery.dto.BoardDTO;
 import com.sunky.gallery.dto.PageRequestDTO;
 import com.sunky.gallery.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board/*")
@@ -24,5 +27,19 @@ public class BoardController {
         log.info("-------------"+boardService.getList(pageRequestDTO));
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..."+dto);
+        Long pno = boardService.register(dto);
+        redirectAttributes.addFlashAttribute("msg",pno);
+
+        return "redirect:/board/list";
     }
 }
