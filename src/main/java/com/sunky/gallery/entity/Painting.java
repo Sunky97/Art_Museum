a26bf522d568eb1338358169de7fd9693440375f
Sunky97 <hsk11261612@gmail.com>
2021-05-21 23:16:49 +0900

    Painting 등록

---
+++ src/main/java/com/sunky/gallery/entity/Painting.java
@@ -0,0 +1,23 @@
+package com.sunky.gallery.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Painting extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    private String title;
+}
