c92073438f663ca969aedbba24ee39ebd4ffaea1
Sunky97 <hsk11261612@gmail.com>
2021-05-21 23:19:46 +0900

    PaintingImage 등록

---
+++ src/main/java/com/sunky/gallery/entity/PaintingImage.java
@@ -0,0 +1,27 @@
+package com.sunky.gallery.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString(exclude = "painting")
+public class PaintingImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Painting painting;
+}
