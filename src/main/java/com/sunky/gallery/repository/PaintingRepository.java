abd6a45c3c3a5a1563f08eff193cf6ad98b79d59
Sunky97 <hsk11261612@gmail.com>
2021-05-22 21:18:20 +0900

    페이징 쿼리문 추가

--- src/main/java/com/sunky/gallery/repository/PaintingRepository.java
+++ src/main/java/com/sunky/gallery/repository/PaintingRepository.java
@@ -2,7 +2,15 @@ package com.sunky.gallery.repository;
 
 import com.sunky.gallery.entity.Painting;
 import com.sunky.gallery.entity.PaintingImage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaintingRepository extends JpaRepository<Painting, Long> {
+
+    @Query("SELECT p, pi, avg(coalesce(r.grade,0)), count(distinct r) FROM Painting p " +
+            "LEFT OUTER JOIN PaintingImage pi on pi.painting = p " +
+            "LEFT OUTER JOIN Review r ON r.painting = p GROUP BY p")
+    Page<Object[]> getListPage(Pageable pageable);
 }
--- src/test/java/com/sunky/gallery/repository/PaintingRepositoryTests.java
+++ src/test/java/com/sunky/gallery/repository/PaintingRepositoryTests.java
@@ -5,9 +5,13 @@ import com.sunky.gallery.entity.PaintingImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -43,5 +47,17 @@ public class PaintingRepositoryTests {
                 imageRepository.save(paintingImage);
             }
         });
+    }
+
+    @Test
+    public void testListpage(){
+
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by(Sort.Direction.DESC,"pno"));
+
+        Page<Object[]> result = paintingRepository.getListPage(pageRequest);
+
+        for(Object[] objects : result.getContent()){
+            System.out.println(Arrays.toString(objects));
+        }
     }
 }
