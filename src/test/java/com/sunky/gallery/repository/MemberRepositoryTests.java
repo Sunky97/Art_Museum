f808654ab5cd2afe08eb561cd43275f6123bd660
Sunky97 <hsk11261612@gmail.com>
2021-05-22 21:02:02 +0900

    MemberRepositoryTests 등록

---
+++ src/test/java/com/sunky/gallery/repository/MemberRepositoryTests.java
@@ -0,0 +1,24 @@
+package com.sunky.gallery.repository;
+
+import com.sunky.gallery.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void 회원_등록(){
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Member member = Member.builder().email("r"+i+"@naver.com").password("1111").nickname("reviewer"+i).build();
+            memberRepository.save(member);
+        });
+    }
+}
