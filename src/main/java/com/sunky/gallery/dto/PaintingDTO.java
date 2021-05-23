f2936b8973c715988fbe94d4ddede3c18725f6f3
Sunky97 <hsk11261612@gmail.com>
2021-05-23 23:17:43 +0900

    PaintingDTO 등록

---
+++ src/main/java/com/sunky/gallery/dto/PaintingDTO.java
@@ -0,0 +1,21 @@
+package com.sunky.gallery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaintingDTO {
+
+    private Long pno;
+    private String title;
+
+    @Builder.Default
+    private List<PaintingImageDTO> imageDTOList = new ArrayList<>();
+}
