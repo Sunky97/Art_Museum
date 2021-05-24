02e1341baf998f509ecf9e8175d4da2944f678cd
Sunky97 <hsk11261612@gmail.com>
2021-05-25 00:19:54 +0900

    ReviewService 등록

---
+++ src/main/java/com/sunky/gallery/service/ReviewService.java
@@ -0,0 +1,48 @@
+package com.sunky.gallery.service;
+
+import com.sunky.gallery.dto.ReviewDTO;
+import com.sunky.gallery.entity.Member;
+import com.sunky.gallery.entity.Painting;
+import com.sunky.gallery.entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    List<ReviewDTO> getListOfPainting(Long pno);
+
+    Long register(ReviewDTO pReviewDTO);
+
+    void modify(ReviewDTO pReviewDTO);
+
+    void remove(Long reviewnum);
+
+    default Review dtoToEntity(ReviewDTO pReviewDTO){
+
+        Review pReview = Review.builder()
+                .reviewnum(pReviewDTO.getReviewnum())
+                .painting(Painting.builder().pno(pReviewDTO.getPno()).build())
+                .member(Member.builder().mid(pReviewDTO.getMid()).build())
+                .grade(pReviewDTO.getGrade())
+                .text(pReviewDTO.getText())
+                .build();
+
+        return pReview;
+    }
+
+    default ReviewDTO entityTODto(Review pReview){
+
+        ReviewDTO pReviewDTO = ReviewDTO.builder()
+                .reviewnum(pReview.getReviewnum())
+                .pno(pReview.getPainting().getPno())
+                .mid(pReview.getMember().getMid())
+                .nickname(pReview.getMember().getEmail())
+                .grade(pReview.getGrade())
+                .text(pReview.getText())
+                .regDate(pReview.getRegDate())
+                .modDate(pReview.getModDate())
+                .build();
+
+        return pReviewDTO;
+    }
+}
