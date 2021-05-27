ffdd6da6ad4ad1901f65fa8a204b31c1afbf50fd
Sunky97 <hsk11261612@gmail.com>
2021-05-27 21:31:10 +0900

    구글로그인 추가

---
+++ src/main/java/com/sunky/gallery/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.sunky.gallery.controller;
+
+import com.sunky.gallery.config.auth.dto.SessionUser;
+import com.sunky.gallery.dto.PageRequestDTO;
+import com.sunky.gallery.service.PaintingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PaintingService paintingService;
+    private HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, PageRequestDTO pageRequestDTO){
+        model.addAttribute("result", paintingService.getList(pageRequestDTO));
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        } else {
+            model.addAttribute("userName", null);
+        }
+
+        return "/painting/list";
+    }
+}
