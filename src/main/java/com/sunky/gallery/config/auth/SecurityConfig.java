4b4e8747655fedfcebe379747982a9b0390e6900
Sunky97 <hsk11261612@gmail.com>
2021-06-03 21:46:01 +0900

    스크롤 이벤트 구현

--- src/main/java/com/sunky/gallery/config/auth/SecurityConfig.java
+++ src/main/java/com/sunky/gallery/config/auth/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().headers().frameOptions().disable().and().authorizeRequests()
-                .antMatchers("/","/css/**","/image/**","/js/**","/h2-console/**","/painting/**","/display/**").permitAll()
+                .antMatchers("/","/css/**","/image/**","/js/**","/h2-console/**","/painting/**","/display/**","/api/getPosts").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.MEMBER.name())
                 .anyRequest().authenticated().and().logout().logoutSuccessUrl("/painting/list")
                 .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
--- src/main/java/com/sunky/gallery/controller/PaintingRestController.java
+++ src/main/java/com/sunky/gallery/controller/PaintingRestController.java
@@ -5,21 +5,29 @@ import com.sunky.gallery.dto.PageResultDTO;
 import com.sunky.gallery.dto.PaintingDTO;
 import com.sunky.gallery.service.PaintingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 public class PaintingRestController {
 
     private final PaintingService paintingService;
 
-    @GetMapping("/api/getPosts")
-    public ResponseEntity<PageResultDTO> getList(PageRequestDTO pageRequestDTO, Model model){
-        PageResultDTO result = paintingService.getList(pageRequestDTO);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    @PostMapping("/api/getPosts")
+    public @ResponseBody PageResultDTO<PaintingDTO, Object[]> getList(@RequestBody Map<String, Object> param){
+
+        PageRequestDTO requestDTO = new PageRequestDTO();
+        requestDTO.setPage((int)param.get("page"));
+        return paintingService.getList(requestDTO);
     }
 }
